2aa8d50b7c844f704b0d5411fca329b5762b5fb9
Ctac <ctac.shamsheev@gmail.com>
2022-08-14 17:11:05 +0700

    simple json add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,62 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Locale;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+    public static void main(String[] args) throws IOException {
+//        FileWriter writer = new FileWriter("out1.json");
+//        writer.write("{\"hello\":1}");
+//        writer.close();
 
         try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println(e);
-        }
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:postgresql://127.0.0.1:5432/test", "postgres", "1")) {
+            JsonReader jsonIn = new JsonReader("in2.json");
+            jsonIn.statParse();
 
-            if (conn != null) {
-                System.out.println("Connected to the database!");
-            } else {
-                System.out.println("Failed to make connection!");
-            }
-
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState().toString(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ParseException | java.text.ParseException e) {
+            System.out.println(e);
         }
+
+//        try {
+//            String SQL_SELECT = "Select * from tast";
+//            Connection conn = getDBConnection();
+//            PreparedStatement preparedStatement = conn.prepareStatement(SQL_SELECT);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            while (resultSet.next()) {
+//                int id = resultSet.getInt("key_id");
+//                String name = resultSet.getString("name");
+//                System.out.println(id + " " + name);
+//            }
+//        } catch (SQLException e) {
+//            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
+
+    public static String DB_DRIVER = "org.postgresql.Driver";
+    public static String DB_CONNECTION = "jdbc:postgresql://127.0.0.1:5432/postgres";
+    public static String DB_USER = "postgres";
+    public static String DB_PASSWORD = "1";
+
+    public static Connection getDBConnection() {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Connection to: " + DB_CONNECTION + " successful");
+        return dbConnection;
+    }
+
 }
