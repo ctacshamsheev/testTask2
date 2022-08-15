31161756f9d82c075df1ff90dc0f5095e6463191
Ctac <ctac.shamsheev@gmail.com>
2022-08-15 23:15:12 +0700

    on idea all ok!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # testTask2
+
 Тестовое задание на соискателя Java Junior Developer aikamsoft.com
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) {
         try {
             ServiceBuilder build = new ServiceBuilder(new ArgsParser(args), DB_DRIVER, DB_CONNECTION, DB_USER, DB_PASSWORD);
-//            ServiceBuilder build = new ServiceBuilder(new ArgsParser(new String[]{"search", "in1.json", "out1.json"}), DB_DRIVER, DB_CONNECTION, DB_USER, DB_PASSWORD);
-//            ServiceBuilder build2 = new ServiceBuilder(new ArgsParser(new String[]{"stat", "in2.json", "out2.json"}), DB_DRIVER, DB_CONNECTION, DB_USER, DB_PASSWORD);
+            //ServiceBuilder build1 = new ServiceBuilder(new ArgsParser(new String[]{"search", "in1.json", "out1.json"}), DB_DRIVER, DB_CONNECTION, DB_USER, DB_PASSWORD);
+            //ServiceBuilder build2 = new ServiceBuilder(new ArgsParser(new String[]{"stat", "in2.json", "out2.json"}), DB_DRIVER, DB_CONNECTION, DB_USER, DB_PASSWORD);
         } catch (IllegalArgumentException | IOException e) {
             System.out.println(e.getMessage());
         }
